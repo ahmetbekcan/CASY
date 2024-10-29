a4fc6c0b060d1c1b0227c6f052620c058c620f40
Ahmet Bekcan <ahmetbekcan@users.noreply.huggingface.co>
2024-10-29 19:27:02 +0000

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,6 +13,8 @@ class Chatbot:
                               without leaving the context. You should not do write any other things than that. If the thing the user given doesn't make sense \
                               just ask the user to elaborate on that. Now, answer or respond to the following." 
             final_prompt = initial_prompt + prompt
+            if (final_prompt[-1] != ".")
+                final_prompt += "."
             response = self.client.text_generation(model=self.model_name, prompt=final_prompt)
             return response
         except Exception as e:
