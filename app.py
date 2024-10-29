ea16eb07243804274f898dc98d71b2e35fb563e2
Ahmet Bekcan <ahmetbekcan@users.noreply.huggingface.co>
2024-10-29 21:45:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,17 @@ st.title("CASY")
 if "messages" not in st.session_state:
     st.session_state.messages = []
     with st.chat_message("assistant"):
-        initial_message = st.write_stream(chatbot.ask_model([{"role": "user", "content": "Hi!"}]))
-        st.session_state.messages.append({"role": "assistant", "content": initial_message })
+        st.session_state.initial_message = st.write_stream(chatbot.ask_model([{"role": "user", "content": "Hi!"}]))
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+if st.session_state.initial_message is not None:
+    st.session_state.messages.append({"role": "assistant", "content": initial_message })
+    st.session_state.initial_message = None
+
 
 # Accept user input
 if prompt := st.chat_input("Your answer"):
