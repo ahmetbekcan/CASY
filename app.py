4a0c7315426dfadf3b1a2051aa00b5028da2941e
Ahmet Bekcan <ahmetbekcan@users.noreply.huggingface.co>
2024-10-29 21:23:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title("CASY")
 
 # Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages.append({"role": "assistant", "content": chatbot.ask_model({"role": "user", "content": "Hi!"})})
+    st.session_state.messages.append({"role": "assistant", "content": chatbot.ask_model([{"role": "user", "content": "Hi!"}])})
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
