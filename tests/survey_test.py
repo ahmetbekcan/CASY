a98ccb8dec2dc2a4c81a0858d63589cae17e7a3d
Ahmet Bekcan <ahmetbekcan26@gmail.com>
2024-12-01 18:20:43 +0300

    merged

--- tests/survey_test.py
+++ tests/survey_test.py
@@ -2,11 +2,17 @@ from models.large_language_models import *
 import random
 from enum import IntEnum
 from utils import read_file
+from dataclasses import dataclass
 
 class PromptType(IntEnum):
     NORMAL = 1
     OFFTOPIC = 2
     UNINFORMATIVE = 3
+
+@dataclass
+class SurveyData:
+    messages: list
+    data: list #{prompt_type (key) : prompt_id (value)}
 
 class SurveySimulator:
     def __init__(self, number_of_questions = 5, normal_answer_weight = 0.8, offtopic_answer_weight = 0.1, uninformative_answer_weight = 0.1, initial_messages = [], agent = "alex"):
@@ -15,10 +21,9 @@ class SurveySimulator:
         self.offtopic_weight = offtopic_answer_weight
         self.uninformative_weight = uninformative_answer_weight
         self._normalize_weights()
-        self.messages = initial_messages
+        self.simulation_data = SurveyData(messages=initial_messages,data=[])
         self.offtopic_bot = self._get_offtopic_bot()
         self.lazy_bot = self._get_lazy_bot()
-        self.simulation_data = []
         self.surveyor = Chatbot()
         self.participant = Agent()
         self.participant.set_instruct("Your goal is to answer survey questions. You should only answer the questions by using your background.\
@@ -31,7 +36,7 @@ class SurveySimulator:
         self.uninformative_weight /= sum
 
     def _get_offtopic_bot(self):
-        bot = Agent()
+        bot = Agent(temperature=0.5, max_tokens=256)
         bot.set_instruct("You are a person who is currently participating in a survey.\
                          You should answer the given questions in an irrelevant manner or ask an irrelevant question to the surveyor.")
         return bot
@@ -42,15 +47,15 @@ class SurveySimulator:
         return bot
     
     def _get_answer_no(self):
-        return sum(1 for item in self.messages if item.get("role") == "user") + 1
+        return sum(1 for item in self.simulation_data.messages if item.get("role") == "user") + 1
 
     def _save_data(self,type):
-        self.simulation_data.append({self._get_answer_no():type})
+        self.simulation_data.data.append({self._get_answer_no():type})
         print(f"{type}: {self._get_answer_no()} \n")
 
     def _simulate_surveyor(self):
-        res = ''.join(self.surveyor.ask_model(self.messages))
-        self.messages.append({"role": "assistant", "content": res})
+        res = ''.join(self.surveyor.ask_model(self.simulation_data.messages))
+        self.simulation_data.messages.append({"role": "assistant", "content": res})
 
     def _simulate_participant(self):
         res = ""
@@ -58,19 +63,19 @@ class SurveySimulator:
         selection = random.choices(list(PromptType), weights=weights, k=1)[0]
 
         if (selection == PromptType.NORMAL):
-            res = self.participant.ask_model_reversed(self.messages)
+            res = self.participant.ask_model_reversed(self.simulation_data.messages)
             self._save_data("normal")
         elif (selection == PromptType.OFFTOPIC):
-            res = self.offtopic_bot.ask_model_reversed(self.messages)
+            res = self.offtopic_bot.ask_model_reversed(self.simulation_data.messages)
             self._save_data("offtopic")
         elif (selection == PromptType.UNINFORMATIVE):
-            res = self.lazy_bot.ask_model_reversed(self.messages)
+            res = self.lazy_bot.ask_model_reversed(self.simulation_data.messages)
             self._save_data("uninformative")
         
-        self.messages.append({"role": "user", "content": res})
+        self.simulation_data.messages.append({"role": "user", "content": res})
 
     def _simulate_q_and_a(self):
-        if (self.messages[-1].get("role") == "assistant" if self.messages else False):
+        if (self.simulation_data.messages[-1].get("role") == "assistant" if self.simulation_data.messages else False):
             self._simulate_participant()
         
         self._simulate_surveyor()
@@ -86,4 +91,18 @@ class SurveySimulator:
             i+=1
     
     def get_simulation_result(self):
-        return self.messages
+        return self.simulation_data.messages
+
+class SurveyEvaluator:
+    def __init__(self, survey_data=None):
+        self.survey_data = survey_data
+
+    def _get_evaluator_agent_params(self):
+        params = {
+            "temperature": 0.1,
+            "max_tokens": 100,
+            "top_p": 0.4,
+            "frequency_penalty": 0.4,
+            "presence_penalty": 0,
+        }
+        return params
